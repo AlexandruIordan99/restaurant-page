c1e36ef1125ac9a95babf8fcdbd30a32f3fb0047
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-19 14:21:07 +0200

    Added contact info

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,7 @@
 export function loadContactPage(){
-
+  const content = document.getElementById("content");
+  const contactDiv = document.createElement("div");
+  contactDiv.innerText = "E-mail: pizza@pizzamail.com \n Phone Number: +500 656 0002";
+  content.appendChild(contactDiv);
 }
 
